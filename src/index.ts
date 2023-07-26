44fea794525751425e26761b21142c601bdaec73
alfiannrzky <alfiannrzky0@gmail.com>
2023-07-26 17:26:31 +0700

    change server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,6 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
-import multer from 'multer'
-import cookieParser from 'cookie-parser'
-import router from './routes/index'
-import sequelize from './config/connection'
+import app from './server'
 
-dotenv.config()
-
-class App {
-  public app: Application
-
-  constructor() {
-    this.app = express()
-    this.plugins()
-    this.routes()
-  }
-
-  protected plugins(): void {
-    this.app.use(cors())
-    this.app.use(morgan('combined'))
-    this.app.use(express.json())
-    this.app.use(bodyParser.json())
-    this.app.use(bodyParser.urlencoded({ extended: false }))
-    this.app.use(multer().none())
-    this.app.use(cookieParser())
-    this.app.use(router)
-  }
-
-  protected routes(): void {
-    this.app.use(router)
-  }
-}
-
-const port = process.env.PORT
-const app = new App().app
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.')
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error)
-  })
+const port = process.env.APP_PORT
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
