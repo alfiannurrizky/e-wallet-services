cbe6e592a6333428f5b246d39bc8f6d8ed0df50c
alfiannrzky <alfiannrzky0@gmail.com>
2023-07-26 17:19:34 +0700

    deleted file

--- src/utils/PasswordHashed.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const passwordHashed = async (password: string) => {
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  return hashedPassword
-}
