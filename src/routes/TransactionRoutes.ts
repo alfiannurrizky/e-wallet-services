610282608c5278eeb5b9a7cb38613301da481ad1
alfiannrzky <alfiannrzky0@gmail.com>
2023-07-27 12:19:08 +0700

    deleted old files

--- src/routes/TransactionRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import TransactionController from '../controllers/TransactionController'
-import { auth } from '../middlewares/AuthMiddleware'
-
-const router: Router = Router()
-
-router.get('/transactions', auth, TransactionController.getTransaction)
-
-export default router
